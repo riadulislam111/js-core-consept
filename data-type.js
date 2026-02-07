c36dbeafb7268c21d8a29b4fc8c895ab1b410c07
Jarif Ahmed <your_github_email@gmail.com>
2026-02-07 18:25:38 +0600

    added all files updates

---
+++ data-type.js
@@ -0,0 +1,18 @@
+// JS is a dynamic typed programming language
+let data = 4;
+data = 'tormuj';
+data = false
+data = { name: 'vaangi', price: 40};
+data = [445];
+data = undefined;
+data = null;
+// console.log(typeof data);
+
+//js: primitive data type
+const price = 50;
+const name = 'bodrul';
+const isPoor = false;
+let isknown = undefined;
+let isLocal;
+const merit = null;
+console.log(isknown, isLocal)
