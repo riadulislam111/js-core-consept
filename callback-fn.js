b26b4478b3787eae386a2f27a0513284e336af6e
Jarif Ahmed <your_github_email@gmail.com>
2026-02-08 23:14:50 +0600

    added all file updates

---
+++ callback-fn.js
@@ -0,0 +1,30 @@
+// function greet(name) {
+//  console.log("Hello" + name);   
+// }
+// function processUser(callBack) {
+//     callBack("Ali");
+// }
+// processUser(greet);
+
+// function calculate(a, b, callBack) {
+//     callBack(a + b);
+// }
+// calculate(5, 3, function(result) {
+//     console.log(result);
+// });
+
+// calculate(5, 3, (result) => {
+//     console.log(result);
+// });
+
+
+// setTimeout(() => {
+//     console.log("This runs after 2 seconds");
+// }, 2000);
+
+// [1, 2, 3].forEach((num) => {
+//   console.log(num);
+// });
+
+
+// const doubled = [1, 2, 3].map(num => num * 2);
--- scope.js
+++ scope.js
@@ -93,8 +93,8 @@
 // test();
 // console.log(x);
 
-for (let i = 0; i < 3; i++) {
-    setTimeout(() => {
-        console.log(i);
-    }, 1000);
-}
+// for (let i = 0; i < 3; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 1000);
+// }
