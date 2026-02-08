ff4278c5cbec2812560775ef6d8d834bbac88732
Jarif Ahmed <your_github_email@gmail.com>
2026-02-08 12:33:54 +0600

    added all files code updates

--- undefined.js
+++ undefined.js
@@ -1,11 +1,32 @@
+function total(a , b) {
+    console.log('value of parameters', a, b);
+   const sum = a + b;
+   console.log('total is', sum)
+}
+// total(2)
+const result = total(5, 7);
+// console.log('value of function call', result)
+
+
 function total2(a , b) {
     console.log('value of parameters', a, b);
-    if (a === undefined || b === undefined) {
-        return;
-    }
-    if (a && b) {
-        const sum = a + b;
-        return sum;
-    }
+   if (a === undefined || b === undefined) {
+    return;
+   }
+   if(a && b) {
+    const sum = a + b;
+    return sum;
+   }
 }
-const result2 = total2(5, 7);
+const result2 = total2(1)
+// console.log('result2', result2)
+
+const phone = { brand: 'samsu', price: 12500 }
+// console.log(phone.color)
+
+const banks = ['sonali', 'rupali', 'jomuna']
+// console.log(banks[2])
+delete banks[1]
+console.log(banks[1])
+console.log('type of undefined', typeof undefined)
+console.log('type of null', typeof null)
