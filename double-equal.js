d2c25e4e3d652137126b6dc174864cb36652999c
Jarif Ahmed <your_github_email@gmail.com>
2026-02-08 16:10:21 +0600

    added all files updates

---
+++ double-equal.js
@@ -0,0 +1,18 @@
+// type quartion ( type convert )
+// console.log(2 == 2);
+// console.log(1 == 2);
+// console.log(2 == 12);
+// console.log(true == '1'); // 1 == '1' --> 1
+// console.log(false == 0);// 0 == '0' --> == 0
+// console.log(null == undefined);
+// console.log(NaN === NaN);
+// console.log(0 == '');
+// console.log([] == '');
+// console.log([5] == 5);
+// console.log({} == {});
+// console.log([5] == [5]);
+// const p = [4 , 5, 7];
+// const q = p;
+// console.log(p == q);
+
+console.log([5].toString())
